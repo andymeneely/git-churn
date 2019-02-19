b76367586a53c14947397d99cadd3808c158393c
Andy Meneely <andy@se.rit.edu>
2019-02-19 00:40:07 -0500

    almost there

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use git2::Repository;
+use git2::DiffOptions;
+use git2::DiffFormat;
 
 fn main() {
     let repo = match Repository::open(".") {
@@ -10,9 +12,22 @@ fn main() {
         Err(e) => panic!("failed to revparse: {}", e),
     };
     let commit  = head.peel_to_commit().unwrap();
+    let commit_tree = match head.peel_to_commit().unwrap().tree() {
+        Ok(commit_tree) => commit_tree,
+        Err(e) => panic!("failed to revparse: {}", e),
+    };
     println!("Commit message is: {}",commit.message().unwrap_or(""));
+    for p in commit.parents(){
+        let p_tree = match p.tree() {
+            Ok(p_tree) => p_tree,
+            Err(e) => panic!("failed to revparse: {}", e),
+        };
+        let mut diff_opts = DiffOptions::new();
+        let diff = repo.diff_tree_to_tree(Some(&p_tree), Some(&commit_tree), Some(&mut diff_opts));
 
-
+        // diff.unwrap().print(DiffFormat::Patch,());
+        // println!("Diff: {:#?}", diff);
+    }
 }
 
 #[cfg(test)]
