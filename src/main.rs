b16a1c9d32afca66c6fa95367525685b950fb387
Andy Meneely <andy@se.rit.edu>
2019-02-18 23:53:12 -0500

    my first rust unit test!! with dogfood

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use git2::Repository;
 
 fn main() {
-    let repo = match Repository::open(".") {
+    let repo = match git2::Repository::open(".") {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
     };
-    let head = match repo.revparse_single("HEAD") {
-        Ok(head) => head,
-        Err(e) => panic!("failed to revparse: {}", e),
-    };
-    println!("Repo head is: {}",head.id());
+    // println!("Repo head is: {}",head.id());
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_goofing_tag() {
+        let repo = match git2::Repository::open(".") {
+            Ok(repo) => repo,
+            Err(e) => panic!("failed to open: {}", e),
+        };
+        let head = match repo.revparse_single("test-goofing") {
+            Ok(head) => head,
+            Err(e) => panic!("failed to revparse: {}", e),
+        };
+
+        assert_eq!("79caa008ba1f9d06b34b4acc7c03d7fade185a63", head.id().to_string());
+    }
 }
