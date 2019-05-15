a1c842ba7e5252ff8e2de1bfe25c4b644548f6a4
Andy Meneely <andy@se.rit.edu>
2019-05-14 23:48:52 -0400

    continue refactoring and learning

--- src/git_churn/mod.rs
+++ src/git_churn/mod.rs
@@ -1,20 +1,69 @@
-pub mod git_churn {
-    pub struct ChurnStats {
-        commits: u32,
-        insertions: u32,
-        deletions: u32,
-        authors: u32,
-        committers: u32,
-        merge_commits: u32,
-        merges: u32,
-        self_deletions: Option<u32>,
-        authors_affected_by_other_deletions: Option<u32>,
+use git2::*;
+
+pub struct ChurnStats {
+    log_str: String,
+    commits: usize,
+    insertions: usize,
+    deletions: usize,
+    files_changed: usize,
+    authors: usize,
+    committers: usize,
+    merge_commits: usize,
+    merges: usize,
+    self_deletions: Option<usize>,
+    authors_affected_by_other_deletions: Option<usize>,
+}
+
+impl ChurnStats {
+    pub fn pretty_print(&self) {
+        println!(
+            r#"
+Git Churn stats for {log_str}
+  {commits:<3} commits
+  {insertions:<3} insertions
+  {deletions:<3} deletions
+  {files_changed:<3} files_changed
+  {authors:<3} authors
+  {committers:<3} committers
+  {merge_commits:<3} merge_commits
+  {merges:<3} merges
+  {self_deletions:?} self deletions
+  {authors_affected_by_other_deletions:?} authors affected by other deletions
+"#,
+            log_str = self.log_str,
+            commits = self.commits,
+            insertions = self.insertions,
+            deletions = self.deletions,
+            files_changed = self.files_changed,
+            authors = self.authors,
+            committers = self.committers,
+            merge_commits = self.merge_commits,
+            merges = self.merges,
+            self_deletions = self.self_deletions,
+            authors_affected_by_other_deletions = self.authors_affected_by_other_deletions,
+        );
     }
 
-    impl ChurnStats {
-        //TODO to_json string
-        // fn to_json() -> String {
-        //
-        // }
+    pub fn new() -> ChurnStats {
+        ChurnStats {
+            log_str: String::from("TEST log"),
+            commits: 0,
+            insertions: 0,
+            deletions: 0,
+            files_changed: 0,
+            authors: 0,
+            committers: 0,
+            merge_commits: 0,
+            merges: 0,
+            self_deletions: None,
+            authors_affected_by_other_deletions: None,
+        }
+    }
+
+    pub fn set(&mut self, diff_stats: DiffStats) -> &mut ChurnStats {
+        self.insertions = diff_stats.insertions();
+        self.deletions = diff_stats.deletions();
+        self.files_changed = diff_stats.files_changed();
+        return self;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,27 @@
+mod git_churn;
+
+use self::git_churn::*;
 use git2::*;
 use std::path::Path;
 
-mod git_churn;
-
-fn compute_churn() {
-    let repo = Repository::open(".").expect("Could not open repo");
-    let head = repo
-        .revparse_single("test-first-test")
-        .expect("Failed revparse");
-    let commit = head.peel_to_commit().expect("Could not peel to commit");
+fn compute_churn(repo: &Repository, commit: &Commit, _interactive: bool) -> ChurnStats {
     let commit_tree = commit.tree().expect("Could not get tree");
+    let mut stats = ChurnStats::new();
     for p in commit.parents() {
         let parent_tree = p.tree().expect("Failed find parent tree");
         let mut diff_opts = init_diff_opts();
         let diff = repo
             .diff_tree_to_tree(Some(&parent_tree), Some(&commit_tree), Some(&mut diff_opts))
-            .unwrap();
+            .expect("Failed to diff tree to tree");
         let diff_stats = diff.stats().expect("Failed to compute diff_stats");
-
-        println!(
-            "Diff stats: {} insertions, {} deletions",
-            diff_stats.insertions(),
-            diff_stats.deletions()
-        );
+        stats.set(diff_stats);
 
         println!("--- DIFF ---");
-        diff.foreach(
-            &mut file_cb,
-            Some(&mut binary_cb),
-            Some(&mut hunk_cb),
-            Some(&mut line_cb),
-        )
-        .expect("Failed to iterate over diff");
+        diff.foreach(&mut file_cb, None, None, Some(&mut line_cb))
+            .expect("Failed to iterate over diff");
 
         println!("-- BLAME --");
-        let mut blame_opts = init_blame_opts(head.id());
+        let mut blame_opts = init_blame_opts(commit.id());
         let path = Path::new("src/main.rs");
         let blame = repo
             .blame_file(&path, Some(&mut blame_opts))
@@ -47,6 +34,7 @@ fn compute_churn() {
             );
         }
     }
+    return stats;
 }
 
 fn init_diff_opts() -> DiffOptions {
@@ -69,18 +57,16 @@ fn init_blame_opts(head_id: Oid) -> BlameOptions {
 }
 
 fn main() {
-    compute_churn();
+    let repo = Repository::open(".").expect("Could not open repo");
+    let commit = repo
+        .revparse_single("test-first-test")
+        .expect("Failed revparse")
+        .peel_to_commit()
+        .expect("Could not peel to commit");
+    compute_churn(&repo, &commit, false).pretty_print();
 }
 
 fn file_cb(_d: DiffDelta, _progress: f32) -> bool {
-    true
-}
-
-fn binary_cb(_d: DiffDelta, _db: DiffBinary) -> bool {
-    true
-}
-
-fn hunk_cb(_d: DiffDelta, _dh: DiffHunk) -> bool {
     true
 }
 
@@ -107,4 +93,5 @@ mod tests {
             head.id().to_string()
         );
     }
+
 }
