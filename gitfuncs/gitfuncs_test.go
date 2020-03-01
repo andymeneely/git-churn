3bbb2962d24fc4207be580bfaeb7d1076d3492ef
ashishgalagali <ashishgalagali@gmail.com>
2020-03-01 17:09:42 -0500

    Deleted binary file and corrected the test case

--- gitfuncs/gitfuncs_test.go
+++ gitfuncs/gitfuncs_test.go
@@ -2,13 +2,15 @@ package gitfuncs
 
 import (
 	"os"
+	"strings"
 	"testing"
 )
 
 func TestBranches(t *testing.T) {
 	dir,_:= os.Getwd()
-	got := Branches(dir)
-	if len(got)>0{
-		t.Errorf("Branches(%q) == %q, want aleast 1 branch", dir, got)
+	rootDit := strings.Replace(dir,"gitfuncs","",1)
+	got := Branches(rootDit)
+	if len(got)<=0{
+		t.Errorf("Branches(%q) == %q, want aleast 1 branch", rootDit, got)
 	}
 }
